c2e1d2ab8a7e21233a8d04d1adad06c240f57719
Markus Schmid <markus@leo.org>
2016-03-09 12:27:00 +0100

    Initial Commit

---
+++ AlertNWait/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertNWait
+//
+//  Created by leodev on 09.03.16.
+//  Copyright © 2016 leodev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
