06624c1a5bd7a3a706eae18945bc67ae5f509790
Markus Schmid <markus@leo.org>
2016-03-10 06:26:30 +0100

    OL in Main Thread. Not working when dispatched.

--- AlertNWait/ViewController.m
+++ AlertNWait/ViewController.m
@@ -14,14 +14,37 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    - (void)viewDidAppear:(BOOL)animated {
+        
+        NSLog(@"Step 1");
+        [self alertAndWait];
+        NSLog(@"Step 4");
+        
+        NSLog(@"Step 1");
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                [self alertAndWait];
+                 NSLog(@"Step 4");
+            });
+        });
+    }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+    - (void)alertAndWait {
+            NSLog(@"Step 2");
+            __block BOOL ok = NO;
+            UIAlertController* myAlert = [UIAlertController alertControllerWithTitle:@"Alert"
+                                                                             message:@"Please press OK to continue"
+                                                                      preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction* continueAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                                   handler:^(UIAlertAction * action) {
+                                                                       ok=YES;
+                                                                   }];
+            [myAlert addAction:continueAction];
+            [self presentViewController:myAlert animated:NO completion:nil];
+            while(!ok) {
+                [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+            };
+            NSLog(@"Step 3");
+    }
 
 @end
